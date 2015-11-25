d534b2a0cedb0fabef41bee9b0a37b7db6603637
shun <929182889@qq.com>
2015-11-25 12:49:37 +0800

    1在最新兼职里添加定位功能

--- app/src/main/java/com/shun/campuswork/fragment/NewsFragment.java
+++ app/src/main/java/com/shun/campuswork/fragment/NewsFragment.java
@@ -1,34 +1,124 @@
 package com.shun.campuswork.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 /**
  * 最新招聘信息的fragment--单例模式
  * Created by shun99 on 2015/11/19.
  */
-public class NewsFragment extends Fragment {
+public class NewsFragment extends Fragment implements AMapLocationListener {
     private static NewsFragment instance = null;
+    private TextView mTextView;
 
     public static NewsFragment getInstance() {
-        if (instance == null){
+        if (instance == null) {
             instance = new NewsFragment();
         }
         return instance;
     }
 
-    private NewsFragment(){}
+    private NewsFragment() {
+    }
 
-    @Nullable
+
+    /**
+     * 定位服务
+     */
+
+    public AMapLocationClient mlocationClient;//声明AMapLocationClient类对象
+    public AMapLocationClientOption mLocationOption;//声明定位参数设置，通过这个类可以对定位的相关参数进行设置
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("这是最新招聘信息");
-        return textView;
+
+
+        mTextView = new TextView(getActivity());
+        mTextView.setText("这是最新招聘信息");
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+        return mTextView;
+    }
+
+
+    public void start() {
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(getContext());
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置是否只定位一次,默认为false
+            mLocationOption.setOnceLocation(true);
+            //设置是否强制刷新WIFI，默认为强制刷新
+            mLocationOption.setWifiActiveScan(true);
+//            //设置是否允许模拟位置,默认为false，不允许模拟位置
+//            mLocationOption.setMockEnable(false);
+//            //设置定位间隔,单位毫秒,默认为2000ms
+//            mLocationOption.setInterval(2000);
+            //给定位客户端对象设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            // 启动定位
+            mlocationClient.startLocation();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                aMapLocation.getLatitude();//获取经度
+                aMapLocation.getLongitude();//获取纬度
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getRoad();//街道信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+                mTextView.setText(aMapLocation.getAddress());
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+                mTextView.setText("AmapError"+"location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
     }
 }
