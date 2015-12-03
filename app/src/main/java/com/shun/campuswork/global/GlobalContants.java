3816d2fd9e995666660c717136d2b5556d6e93cc
shun <929182889@qq.com>
2015-12-03 08:55:45 +0800

    1完成短信验证功能

--- app/src/main/java/com/shun/campuswork/global/GlobalContants.java
+++ app/src/main/java/com/shun/campuswork/global/GlobalContants.java
@@ -31,5 +31,23 @@ public class GlobalContants {
 		return salary +"元/小时";
 	}
 
+	public static final String[] AVATARS = {
+			"http://tupian.qqjay.com/u/2011/0729/e755c434c91fed9f6f73152731788cb3.jpg",
+			"http://99touxiang.com/public/upload/nvsheng/125/27-011820_433.jpg",
+			"http://img1.touxiang.cn/uploads/allimg/111029/2330264224-36.png",
+			"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339485237265.jpg",
+			"http://diy.qqjay.com/u/files/2012/0523/f466c38e1c6c99ee2d6cd7746207a97a.jpg",
+			"http://img1.touxiang.cn/uploads/20121224/24-054837_708.jpg",
+			"http://img1.touxiang.cn/uploads/20121212/12-060125_658.jpg",
+			"http://img1.touxiang.cn/uploads/20130608/08-054059_703.jpg",
+			"http://diy.qqjay.com/u2/2013/0422/fadc08459b1ef5fc1ea6b5b8d22e44b4.jpg",
+			"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339510584349.jpg",
+			"http://img1.touxiang.cn/uploads/20130515/15-080722_514.jpg",
+			"http://diy.qqjay.com/u2/2013/0401/4355c29b30d295b26da6f242a65bcaad.jpg"
+	};
+
+	public static String getAvatars(int id){
+		return AVATARS[id];
+	}
 
 }
